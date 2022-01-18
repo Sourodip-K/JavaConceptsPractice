4276ecfde3877a291409242b51c49210d079b620
Sourodip Kundu <sourodipkundu.w@gmail.com>
2022-01-18 23:25:52 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,50 @@
+/** Program1
+ * //WE ARE LIVING IN A COMPUTER DOMINATED WORLD
+    //E
+    //There are 4 occurances of E
+ */
+
+
+/** Program 2
+ * //RAINWATER HARVESTING IS ORGANISED BY KMC
+    //frequency of vowels
+    //frequency of A:
+    //frequency of e:
+    //i
+    //o
+    //frequency of u:
+ */
+
+/**
+ * Input: MOHANDAS KARAMCHAND GANDHI. mohandas karmachand gandhi, mOhAnChand
+ * Output1: Mohandas Karamchand Gandhi
+ * Output 2: GANDHI KARAMCHAND MOHANDAS
+ * 
+ * 
+ * 
+ * 
+ */
+import java.util.*;
+class Strings {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        input=" "+input;
+        
+        /**
+         * String arr="AEIOU";
+            int len=input.length();
+            int frequency=0;
+            for(int j=0; j<arr.length();j++) {
+            char ch=arr.charAt(j);
+            for(int i=0; i<len; i++) {
+                char c = input.charAt(i);
+                if(c==ch)
+                    frequency++;
+            }   
+            System.out.println("The occcurance of "+ch+" is: "+frequency);
+            frequency=0;
+        }
+         */
+    }
+}
